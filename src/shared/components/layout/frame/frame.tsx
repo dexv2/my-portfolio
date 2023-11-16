0f6a0e7db6b3f40afbcbf0fdabacb291b86cbdd2
phil002109 <vermontphil.paguiligan@gmail.com>
2023-11-16 17:54:38 +0800

    Replaced og url

--- src/shared/components/layout/frame/frame.tsx
+++ src/shared/components/layout/frame/frame.tsx
@@ -31,12 +31,12 @@ function VpFrame({
         <meta property='og:description' content={description} />
         <meta property='og:type' content='website' />
         <meta property='og:site_name' content={title} />
-        <meta property='og:image' content='https://vermontpaguiligan.com/assets/icons/icon-512x512.png' />
-        <meta property='og:url' content='https://vermontpaguiligan.com/' />
+        <meta property='og:image' content='https://vermont-phil-paguiligan.vercel.app/assets/icons/icon-512x512.png' />
+        <meta property='og:url' content='https://vermont-phil-paguiligan.vercel.app/' />
         <meta name='twitter:title' content={title} />
         <meta name='twitter:description' content={description} />
-        <meta name='twitter:image' content='https://vermontpaguiligan.com/assets/icons/icon-512x512.png' />
-        <meta name='twitter:url' content='https://vermontpaguiligan.com/' />
+        <meta name='twitter:image' content='https://vermont-phil-paguiligan.vercel.app/assets/icons/icon-512x512.png' />
+        <meta name='twitter:url' content='https://vermont-phil-paguiligan.vercel.app/' />
         <meta name='twitter:card' content='summary' />
         <meta name='twitter:creator' content={title} />
       </Head>
